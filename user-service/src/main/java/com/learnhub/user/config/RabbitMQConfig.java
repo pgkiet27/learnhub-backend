e6f0d83c88c8037c5a6443ab7d7ffc78fd2cef34
Pham Kiet <ryanchowin123@gmail.com>
2026-07-06 17:16:40 +0700

    feat: create config files for Spring Security (disable checking requests) and RabbitMQ

---
+++ user-service/src/main/java/com/learnhub/user/config/RabbitMQConfig.java
@@ -0,0 +1,36 @@
+package com.learnhub.user.config;
+
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    // exchange use for all services
+    public static final String EXCHANGE_NAME = "learnhub.events";
+
+    // exchange
+    @Bean
+    public TopicExchange learnhubExchange() {
+        return ExchangeBuilder.topicExchange(EXCHANGE_NAME).durable(true).build();
+    }
+
+    // message converter
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+}
