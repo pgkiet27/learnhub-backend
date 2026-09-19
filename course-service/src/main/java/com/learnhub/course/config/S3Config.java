cef13ff9a0b0e454b37a5abdfb6f988cdd693bb4
Pham Kiet <ryanchowin123@gmail.com>
2026-09-19 16:48:42 +0700

    fix(course-service): load per-service .env and switch course-service S3 auth to AWS SSO profile

--- course-service/src/main/java/com/learnhub/course/config/S3Config.java
+++ course-service/src/main/java/com/learnhub/course/config/S3Config.java
@@ -3,8 +3,9 @@ package com.learnhub.course.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import org.springframework.util.StringUtils;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -15,27 +16,31 @@ public class S3Config {
     @Value("${aws.region}")
     private String region;
 
-    @Value("${aws.credentials.access-key-id}")
-    private String accessKeyId;
-
-    @Value("${aws.credentials.secret-access-key}")
-    private String secretAccessKey;
+    @Value("${aws.profile:}")
+    private String profile;
 
     @Bean
-    public S3Client s3Client() {
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        DefaultCredentialsProvider.Builder builder = DefaultCredentialsProvider.builder();
+        if (StringUtils.hasText(profile)) {
+            builder.profileName(profile);
+        }
+        return builder.build();
+    }
+
+    @Bean
+    public S3Client s3Client(AwsCredentialsProvider credentialsProvider) {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
     @Bean
-    public S3Presigner s3Presigner() {
+    public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider) {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
